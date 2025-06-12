85540ad8df964be48ee20c969e6c930b74c7ed92
Bruno Correr <bruno.correr.coa@gmail.com>
2025-06-11 21:36:33 -0300

    Criando pop ups dos exercicios

--- Página_inicial/index.js
+++ Página_inicial/index.js
@@ -1,3 +1,6 @@
-let onReload = () => {
-    alert('JS funcionando')
+let openModal = (id) => {
+    let modal = document.getElementById(id).style.display = 'flex';
+    }
+let closeModal = (id) => {
+    let modal = document.getElementById(id).style.display = 'none';
 }
